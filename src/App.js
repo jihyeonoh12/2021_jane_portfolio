8a1c50ddc6167192fc2d7e4bad74b450a7ddaeb3
jihyeonoh12 <jihyeonoh12@gmail.com>
2021-05-02 20:35:10 -0700

    post is working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import {Switch, Route} from 'react-router-dom';
+import Navbar from './components/Navbar'
+
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
-import {Switch, Route} from 'react-router-dom';
-import Navbar from './components/Navbar'
+import PostProteinPlus from './pages/PostProteinPlus';
+
 
 
 function App() {
@@ -14,6 +17,7 @@ function App() {
         <Route exact path="/"><Home /></Route>
         <Route exact path="/contact"><Contact /></Route>
         <Route exact path="/about"><About /></Route>
+        <Route exact path="/post-protein-plus"><PostProteinPlus /></Route>
       </Switch>
     </div>
   );
--- src/pages/PostProteinPlus.js
+++ src/pages/PostProteinPlus.js
@@ -1,14 +1,14 @@
 
 import React from 'react';
 import PostHero from '../components/PostHero'
-import thumb_protein_plus from '../images/protein_plus/thumbnail_protein_plus.jpg';
+import Hero_protein_plus from '../images/protein_plus/2020 Protein Plus - hero.jpg';
 
 class PostProteinPlus extends React.Component {
     render() {
         return (
             <div className="post_body">
             <div className="post_container">
-                <PostHero image={thumb_protein_plus} title="Protein+ Meal Plan Page" description="Research and redesign of service page that tailored for its target audience"/>
+                <PostHero image={Hero_protein_plus} title="Protein+ Meal Plan Page" description="Research and redesign of service page that tailored for its target audience"/>
                 </div>
             </div>
         )
