5c9a435654835a458b3ff3ff098896aee2ce4ba9
jihyeonoh12 <jihyeonoh12@gmail.com>
2021-10-24 18:06:15 -0700

    added password page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import './styles/password.css';
+
 import {Switch, Route} from 'react-router-dom';
 import Navbar from './components/Navbar'
 
@@ -7,10 +9,16 @@ import About from './pages/About';
 import Contact from './pages/Contact';
 import PostProteinPlus from './pages/PostProteinPlus';
 
+import Protect from 'react-app-protect'
+import 'react-app-protect/dist/index.css'
+
 
 
 function App() {
   return (
+
+    <Protect sha512='fb31e21722dd473649f6744d8040bd51199b418c4196e10b5fb9df9abd09a416603807ae032086c76a238bc08c1fb2e3d5b89ceae5e05d5dbf1a655167c66f0f'>
+
     <div className="App">
       <Navbar />
       <Switch>
@@ -20,6 +28,8 @@ function App() {
         <Route exact path="/post-protein-plus"><PostProteinPlus /></Route>
       </Switch>
     </div>
+
+    </Protect>
   );
 }
 
--- src/components/Herobanner.js
+++ src/components/Herobanner.js
@@ -7,7 +7,7 @@ function Herobanner () {
         <div className="hero">
             <div className="hero_container">
                 <h1>
-                Hi I’m Jane. Welcome to my portfolio.
+                Hi I’m Janeee. Welcome to my portfolio.
                 </h1>
                 <p className="top-40">I’m UI/UX designer who loves animals.🐕‍ I like helping users out by finding creative solutions and blowing their mind with pixel perfect design.✨</p>
             </div>
