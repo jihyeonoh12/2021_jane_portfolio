42e197a37d8ba8dfc64503d88dcbf903f1e336c6
jihyeonoh12 <jihyeonoh12@gmail.com>
2024-02-19 22:17:14 -0800

    converted card component to functional component from class-based

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,22 +2,19 @@ import React from 'react'
 import '../styles/general.css';
 import '../styles/card.css';
 
-
-class Card extends React.Component {
-    render() {
-        return(
-            <div className="card">
-                <img src={this.props.image} className="card_image"/>
-                <div className="card_text">
-                    <h2>
-                        {this.props.title}
-                    </h2>
-                    <p>{this.props.description}</p>
-                </div>
+const Card = (props) => {
+    const { image, title, description } = props;
+    return (
+        <div className="card">
+            <img src={image} className="card_image"/>
+            <div className="card_text">
+                <h2>
+                    {title}
+                </h2>
+                <p>{description}</p>
             </div>
-        )
-    }
-    
-}
+        </div>
+    );
+  };
 
 export default Card
