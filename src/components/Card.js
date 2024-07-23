e0d7c2dc38679523aa4824515ad93ac9ffa7b0fb
jihyeonoh12 <jihyeonoh12@gmail.com>
2024-07-22 23:07:29 -0700

    updated card layout

--- src/components/Card.js
+++ src/components/Card.js
@@ -21,7 +21,7 @@ const Card = (props) => {
     return (
         <div className="pt-0 px-0 pb-[20px] mx-2.5 my-[40px] relative">
             <h5 className={'absolute top-1 left-1 p-1 px-2 rounded capitalize ' + preheaderTag}>{preheader}</h5>
-            <img src={image} alt="card" className="object-cover shadow-card rounded-lg mb-2.5 w-full h-[350px]"/>
+            <img src={image} alt="card" className="object-cover shadow-card rounded-lg mb-2.5 w-full h-[180px] md:h-[350px]"/>
             <div className="p-2.5 text-left">
                 <h3>
                     {title}
--- src/components/PostOutcome.js
+++ src/components/PostOutcome.js
@@ -4,11 +4,11 @@ import React from 'react';
 const PostOutcome = ({ image, title, body, url, classes, button }) => {
         return (
            <div className="max-w-[1200px] w-full mx-auto my-[60px] container">
-                <div className="outcome-cotent flex gap-2.5 space-between">
-                    <div className="w-[30%]">
-                        <img src={image} className="w-[800px] w-full max-h-[300px] h-full"/>
+                <div className="outcome-cotent md:flex gap-2.5 space-between">
+                    <div className="md:w-[30%]">
+                        <img src={image} className="w-[800px] w-full md:max-h-[300px] h-full"/>
                     </div>
-                    <div className="m-auto py-[40px] px-[20px] md:px-[60px] text-left w-[70%]">
+                    <div className="m-auto py-[40px] px-[20px] md:px-[60px] text-left md:w-[70%]">
                         <h2>{title}</h2>
                         <p>{body}</p>
                         {url && button ? (
